e608bc7f738e508b762571603b46eb97a5819410
David Rooney <dave69@gmail.com>
2013-03-10 21:52:04 -0500

    Fix for jquery bug that hid menu
    If user hid menu at mobile size, then resized browser to desktop size,
    menu would be hidden. Not an issue for actual mobile users but wanted to
    address the issue anyway.

--- js/site.js
+++ js/site.js
@@ -7,5 +7,20 @@
 				$('nav[role="navigation"]').slideToggle('slow');
 			});
 		});
+		
+		// if window is resized and broswer changes from mobile to desktop, need to be sure to display desktop even uf user has hidden via above
+		$(window).resize(function(){
+			var w = $(window).width();
+			if(w >= 720 && $('nav[role="navigation"]').is(':hidden')) {
+				$('nav[role="navigation"]').show();
+			}
+        });
+		
+		$(window).resize(function(){
+			var w = $(window).width();
+			if(w < 720) {
+				$('nav[role="navigation"]').hide();
+			}
+        });
 
 	});
